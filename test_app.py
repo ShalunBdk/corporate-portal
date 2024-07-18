0903a89b1b9572079800b96e8fe306dd3027eae5
Шалин Александр Русланович <shalin-ar@tion.ru>
2024-07-18 10:03:04 +0700

    add pytest

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import pytest
+from app import app
+
+def test_app():
+    response = app.test_client().get('/api/employees_hierarchy')
+    assert response.status_code == 200
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
